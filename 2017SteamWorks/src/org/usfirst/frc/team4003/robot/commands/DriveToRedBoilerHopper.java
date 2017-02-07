b31f3a4bb5a0f2a0775c4e0b3595dcc1673fcb45
davidaustinm <davidaustinm@gmail.com>
2017-02-06 19:15:55 -0500

    uyiuyiyu

--- 2017SteamWorks/src/org/usfirst/frc/team4003/robot/commands/DriveToRedBoilerHopper.java
+++ 2017SteamWorks/src/org/usfirst/frc/team4003/robot/commands/DriveToRedBoilerHopper.java
@@ -10,8 +10,8 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class DriveToRedBoilerHopper extends CommandGroup {
 
     public DriveToRedBoilerHopper() {
-    	addSequential(new DriveToPoint(100,0, new Acceleration(.2,.6,0.02),true,20));
+    	addSequential(new DriveToPoint(80,0, new Acceleration(.2,.6,0.02),true,20));
     	addSequential(new RotateToHeading(-90,.5,0));
-    	addSequential(new DriveToPoint(100,30, new Acceleration(.3,.3,0)));
+    	addSequential(new DriveToPoint(90,-30, new Acceleration(.3,.3,0)));
     }
 }
