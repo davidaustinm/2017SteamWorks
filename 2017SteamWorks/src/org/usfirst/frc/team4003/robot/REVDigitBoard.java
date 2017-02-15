bb9737bbe2690d347ac762544c171239b78fb201
Justin Buist <justin.buist@gmail.com>
2017-02-14 21:02:24 -0500

    Macroed the commented out code into our Java format.

--- 2017SteamWorks/src/org/usfirst/frc/team4003/robot/REVDigitBoard.java
+++ 2017SteamWorks/src/org/usfirst/frc/team4003/robot/REVDigitBoard.java
@@ -57,59 +57,32 @@ public class REVDigitBoard {
     	charRegistry.put("9", new byte[]{ (byte)0b11101111, (byte)0b00000000 });
     	charRegistry.put("0", new byte[]{ (byte)0b00111111, (byte)0b00000000 });
     	charRegistry.put("A", new byte[]{ (byte)0b11110111, (byte)0b00000000 });
-    	/*
-        charreg[11][0] = (byte)0b10001111; charreg[11][1] = (byte)0b00010010; //B
-        charmap.put('B',11);
-        charreg[12][0] = (byte)0b00111001; charreg[12][1] = (byte)0b00000000; //C
-        charmap.put('C',12);
-        charreg[13][0] = (byte)0b00001111; charreg[13][1] = (byte)0b00010010; //D
-        charmap.put('D',13);
-        charreg[14][0] = (byte)0b11111001; charreg[14][1] = (byte)0b00000000; //E
-        charmap.put('E',14);
-        charreg[15][0] = (byte)0b11110001; charreg[15][1] = (byte)0b00000000; //F
-        charmap.put('F',15);
-        charreg[16][0] = (byte)0b10111101; charreg[16][1] = (byte)0b00000000; //G
-        charmap.put('G',16);
-        charreg[17][0] = (byte)0b11110110; charreg[17][1] = (byte)0b00000000; //H
-        charmap.put('H',17);
-        charreg[18][0] = (byte)0b00001001; charreg[18][1] = (byte)0b00010010; //I
-        charmap.put('I',18);
-        charreg[19][0] = (byte)0b00011110; charreg[19][1] = (byte)0b00000000; //J
-        charmap.put('J',19);
-        charreg[20][0] = (byte)0b01110000; charreg[20][1] = (byte)0b00001100; //K
-        charmap.put('K',20);
-        charreg[21][0] = (byte)0b00111000; charreg[21][1] = (byte)0b00000000; //L
-        charmap.put('L',21);
-        charreg[22][0] = (byte)0b00110110; charreg[22][1] = (byte)0b00000101; //M
-        charmap.put('M',22);
-        charreg[23][0] = (byte)0b00110110; charreg[23][1] = (byte)0b00001001; //N
-        charmap.put('N',23);
-        charreg[24][0] = (byte)0b00111111; charreg[24][1] = (byte)0b00000000; //O
-        charmap.put('O',24);
-        charreg[25][0] = (byte)0b11110011; charreg[25][1] = (byte)0b00000000; //P
-        charmap.put('P',25);
-        charreg[26][0] = (byte)0b00111111; charreg[26][1] = (byte)0b00001000; //Q
-        charmap.put('Q',26);
-        charreg[27][0] = (byte)0b11110011; charreg[27][1] = (byte)0b00001000; //R
-        charmap.put('R',27);
-        charreg[28][0] = (byte)0b10001101; charreg[28][1] = (byte)0b00000001; //S
-        charmap.put('S',28);
-        charreg[29][0] = (byte)0b00000001; charreg[29][1] = (byte)0b00010010; //T
-        charmap.put('T',29);
-        charreg[30][0] = (byte)0b00111110; charreg[30][1] = (byte)0b00000000; //U
-        charmap.put('U',30);
-        charreg[31][0] = (byte)0b00110000; charreg[31][1] = (byte)0b00100100; //V
-        charmap.put('V',31);
-        charreg[32][0] = (byte)0b00110110; charreg[32][1] = (byte)0b00101000; //W
-        charmap.put('W',32);
-        charreg[33][0] = (byte)0b00000000; charreg[33][1] = (byte)0b00101101; //X
-        charmap.put('X',33);
-        charreg[34][0] = (byte)0b00000000; charreg[34][1] = (byte)0b00010101; //Y
-        charmap.put('Y',34);
-        charreg[35][0] = (byte)0b00001001; charreg[35][1] = (byte)0b00100100; //Z
-        charmap.put('Z',35);
-        charreg[36][0] = (byte)0b00000000; charreg[36][1] = (byte)0b00000000; //space
-        */
+        charRegistry.put("B", new byte[]{ (byte)0b10001111, (byte)0b00010010 });
+        charRegistry.put("C", new byte[]{ (byte)0b00111001, (byte)0b00000000 });
+        charRegistry.put("D", new byte[]{ (byte)0b00001111, (byte)0b00010010 });
+        charRegistry.put("E", new byte[]{ (byte)0b11111001, (byte)0b00000000 });
+        charRegistry.put("F", new byte[]{ (byte)0b11110001, (byte)0b00000000 });
+        charRegistry.put("G", new byte[]{ (byte)0b10111101, (byte)0b00000000 });
+        charRegistry.put("H", new byte[]{ (byte)0b11110110, (byte)0b00000000 });
+        charRegistry.put("I", new byte[]{ (byte)0b00001001, (byte)0b00010010 });
+        charRegistry.put("J", new byte[]{ (byte)0b00011110, (byte)0b00000000 });
+        charRegistry.put("K", new byte[]{ (byte)0b01110000, (byte)0b00001100 });
+        charRegistry.put("L", new byte[]{ (byte)0b00111000, (byte)0b00000000 });
+        charRegistry.put("M", new byte[]{ (byte)0b00110110, (byte)0b00000101 });
+        charRegistry.put("N", new byte[]{ (byte)0b00110110, (byte)0b00001001 });
+        charRegistry.put("O", new byte[]{ (byte)0b00111111, (byte)0b00000000 });
+        charRegistry.put("P", new byte[]{ (byte)0b11110011, (byte)0b00000000 });
+        charRegistry.put("Q", new byte[]{ (byte)0b00111111, (byte)0b00001000 });
+        charRegistry.put("R", new byte[]{ (byte)0b11110011, (byte)0b00001000 });
+        charRegistry.put("S", new byte[]{ (byte)0b10001101, (byte)0b00000001 });
+        charRegistry.put("T", new byte[]{ (byte)0b00000001, (byte)0b00010010 });
+        charRegistry.put("U", new byte[]{ (byte)0b00111110, (byte)0b00000000 });
+        charRegistry.put("V", new byte[]{ (byte)0b00110000, (byte)0b00100100 });
+        charRegistry.put("W", new byte[]{ (byte)0b00110110, (byte)0b00101000 });
+        charRegistry.put("X", new byte[]{ (byte)0b00000000, (byte)0b00101101 });
+        charRegistry.put("Y", new byte[]{ (byte)0b00000000, (byte)0b00010101 });
+        charRegistry.put("Z", new byte[]{ (byte)0b00001001, (byte)0b00100100 });
+        charRegistry.put(" ", new byte[]{ (byte)0b00000000, (byte)0b00000000 });
     	
     	chars = new byte[10];
 	}
